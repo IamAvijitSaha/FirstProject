d8e48fa3823e55bfa063cf0c930888c1398a6817
Avijit Saha <avijit9999.sahara@gmail.com>
2024-09-22 01:07:19 +0530

    This is my first push

--- src/NEW/First/TC001.java
+++ src/NEW/First/TC001.java
@@ -5,5 +5,13 @@ public class TC001 {
        System.out.println("\n" +
                " Hello World everyone");
        System.out.println(2+2);
+
+/*
+       System.out.println(2*2);
+       System.out.println(2*4);
+       System.out.println(2*8);
+       System.out.println(2*6);
+
+ */
    }
 }
