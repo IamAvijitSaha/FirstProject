255a5032988b80dd3cf1115333f8c790ea05fa4e
Avijit Saha <avijit9999.sahara@gmail.com>
2024-09-26 02:11:33 +0530

    This is my first push

---
+++ src/NEW/First/TC_DataType.java
@@ -0,0 +1,21 @@
+package NEW.First;
+
+public class TC_DataType {
+    public static void main(String[] args) {
+
+        //Primitive data type
+
+        byte B=100;// 1 bytes,8 bits
+        short S=128;// 2 bytes,16 bits
+        int I=1000;// 4 bytes,32 bits
+        long My_number= 8336010016L;// 8 bytes,64 bits
+        char Grade='A';// 2 bytes,16 bits
+        float F=2.11F;// 4 bytes,32 bits
+        double D=11.114432425;//8 Bytes,64 bits
+        System.out.println("name");
+
+        //Non-Primitive data type
+
+        String name= "Avijit" ; //String= Bunch of data collection
+    }
+}
---
+++ src/NEW/First/TC_DataType_Char.java
@@ -0,0 +1,13 @@
+package NEW.First;
+
+public class TC_DataType_Char {
+    public static void main(String[] args) {
+
+
+char A='$';
+char b='_';
+char c= '\u1F6A';//uni code
+
+System.out.println(c);
+    }
+}
